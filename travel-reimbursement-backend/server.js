40d42ed3336254e3eef8df0c9fbeb3c7866b57b3
Aravind <Aravindratnam1996@gmail.com>
2025-06-28 17:37:40 +0530

    Update server.js

--- travel-reimbursement-backend/server.js
+++ travel-reimbursement-backend/server.js
@@ -14,7 +14,10 @@ const payoutRoutes = require("./routes/payoutRoutes");
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: ["https://travel-reimbursement-system.vercel.app"],
+  credentials: true
+}));
 app.use("/uploads", express.static("uploads"));
 
 //   API route setup
