4ebaf83b754b7305e988b2b993b3ba14f59c4ed2
Aravind <Aravindratnam1996@gmail.com>
2025-06-28 16:02:34 +0530

    Update authAPI.js

--- travel-reimbursement-frontend/src/features/auth/authAPI.js
+++ travel-reimbursement-frontend/src/features/auth/authAPI.js
@@ -1,15 +1,18 @@
+// src/api/authApi.js
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000/api/auth";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL + "/api";
 
 export const meApi = async () => {
-  const res = await axios.get(`${BASE_URL}/me`);
-  return res.data;
-};
-export const logoutApi = async () => {
-  const res = await axios.post(`${BASE_URL}/logout`);
+  const res = await axios.get(`${BASE_URL}/auth/me`);
   return res.data;
 };
 
-export const loginApi = (credentials) => axios.post("/api/login", credentials);
-export const registerApi = (userData) => axios.post("/api/register", userData);
+export const logoutApi = async () => {
+  const res = await axios.post(`${BASE_URL}/auth/logout`);
+  return res.data;
+};
+
+export const loginApi = (credentials) => axios.post(`${BASE_URL}/login`, credentials);
+
+export const registerApi = (userData) => axios.post(`${BASE_URL}/register`, userData);
